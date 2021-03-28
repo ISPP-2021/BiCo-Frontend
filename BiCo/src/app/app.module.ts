bb673dfb664bb70d68c4bb700dba33e784dc8cb9
María Jesús Cadenas Sánchez <mariajesuscadenas@gmail.com>
2021-03-29 00:29:52 +0200

    Style - Inicio y perfil

--- BiCo/src/app/app.module.ts
+++ BiCo/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { RouterModule, Routes } from  '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CercaDeMiComponent } from './components/cerca-de-mi/cerca-de-mi.component';
@@ -13,7 +13,13 @@ import { PerfilComponent } from './components/perfil/perfil.component';
 import { HomeComponent } from './components/home/home.component';
 import { VerNegocioComponent } from './components/negocios/ver-negocio/ver-negocio.component';
 import { GeocodingService } from './services/geocoding-service/geocoding.service';
-import { EditarNegocioComponent } from './components/negocios/editar-negocio/editar-negocio.component'
+import { EditarNegocioComponent } from './components/negocios/editar-negocio/editar-negocio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon'
+import { MatButtonModule} from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import { MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -34,7 +40,14 @@ import { EditarNegocioComponent } from './components/negocios/editar-negocio/edi
     BrowserModule,
     GoogleMapsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+
   ],
   providers: [GeocodingService],
   bootstrap: [AppComponent]
