a271f3d4241885cb8f4e5c1027074dafdb297ddf
augmb <augustomartinez2a@gmail.com>
2021-04-04 03:53:12 +0200

    añadidas cabeceras

--- BiCo/src/app/services/negocio-service/negocio.service.ts
+++ BiCo/src/app/services/negocio-service/negocio.service.ts
@@ -20,7 +20,7 @@ export class NegocioService {
 
   findAll(): Observable<Negocio[]> {
     return this.http
-      .get<Negocio[]>(this.url + 'business')
+      .get<Negocio[]>(this.url + 'business',this.headers)
       .pipe(catchError(this.errorHandler));
   }
 
@@ -30,7 +30,7 @@ export class NegocioService {
 
   findOne(id: Number): Observable<Negocio> {
     return this.http
-      .get<Negocio>(this.url + 'business/' + id)
+      .get<Negocio>(this.url + 'business/' + id, this.headers)
       .pipe(map((negocio: Negocio) => negocio));
   }
 
