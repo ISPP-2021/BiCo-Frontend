7faa21b329dcc7661bc3494a1f60079389aec927
manpercar1 <manuelperezcarrillo@yahoo.es>
2021-04-11 18:01:48 +0200

    Error en el test de mis-negocios

--- BiCo/src/app/components/negocios/mis-negocios/mis-negocios.component.spec.ts
+++ BiCo/src/app/components/negocios/mis-negocios/mis-negocios.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppModule } from 'src/app/app.module';
 
 import { MisNegociosComponent } from './mis-negocios.component';
 
@@ -8,7 +9,10 @@ describe('MisNegociosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MisNegociosComponent ]
+      declarations: [ MisNegociosComponent ],
+      imports: [
+        AppModule
+      ]
     })
     .compileComponents();
   });
