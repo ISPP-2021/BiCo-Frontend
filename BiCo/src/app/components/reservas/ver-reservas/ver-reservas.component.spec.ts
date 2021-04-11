baa8cba542f0b0757b4134783459d8352ca06cee
PepeRodriAdams <josordada@alum.us.es>
2021-04-11 16:44:57 +0200

    Test bug-fix

--- BiCo/src/app/components/reservas/ver-reservas/ver-reservas.component.spec.ts
+++ BiCo/src/app/components/reservas/ver-reservas/ver-reservas.component.spec.ts
@@ -1,25 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VerReservasComponent } from './ver-reservas.component';
+import { AppModule } from 'src/app/app.module';
 
 describe('VerReservasComponent', () => {
-  let component: VerReservasComponent;
-  let fixture: ComponentFixture<VerReservasComponent>;
+	let component: VerReservasComponent;
+	let fixture: ComponentFixture<VerReservasComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ VerReservasComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [VerReservasComponent],
+			imports: [AppModule]
+		})
+			.compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(VerReservasComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(VerReservasComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		localStorage.setItem('token', '12345');
+		expect(component).toBeTruthy();
+	});
 });
