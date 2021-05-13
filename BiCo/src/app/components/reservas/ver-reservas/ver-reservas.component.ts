35551e98d1b4c59c9e379e73bb9da7588c364df6
Pepe <pepecesargarcia@gmail.com>
2021-05-13 16:17:39 +0200

    Cambiar vista

--- BiCo/src/app/components/reservas/ver-reservas/ver-reservas.component.ts
+++ BiCo/src/app/components/reservas/ver-reservas/ver-reservas.component.ts
@@ -10,18 +10,46 @@ import { NegocioService } from 'src/app/services/negocio-service/negocio.service
 import { Servicio } from 'src/app/model/service.interface';
 import { MatTableDataSource } from '@angular/material/table';
 
+export interface PeriodicElement {
+	name: string;
+	position: number;
+	weight: number;
+	symbol: string;
+  }
+  
+  const ELEMENT_DATA: PeriodicElement[] = [
+	{position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+	{position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+	{position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+	{position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+	{position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+	{position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+	{position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+	{position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+	{position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+	{position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
+  
+
 @Component({
 	selector: 'app-ver-reservas',
 	templateUrl: './ver-reservas.component.html',
 	styleUrls: ['./ver-reservas.component.css'],
 })
 export class VerReservasComponent implements OnInit {
-	setData(consumer: Consumer) {
+	async setData(consumer: Consumer) {
 		consumer.bookings.forEach(booking => {
 			this.negocioService.findService(new Number(booking.servise)).subscribe(service =>{
 				booking.service = service;
 				this.negocioService.findOne(new Number(booking.service.business)).subscribe(negocio =>{
 					booking.negocio = negocio;
+					let book = {
+						bussiness: booking.negocio,
+						service: booking.service,
+						day: booking.bookDate,
+						price: booking.service.price
+					}
+					this.data.push(book)
 				})
 			})
 		})		
@@ -34,14 +62,12 @@ export class VerReservasComponent implements OnInit {
 				.findOne()
 				.pipe(map((consumer: Consumer) => {
 					consumer.bookings.sort(this.bookingComparator);
-					this.setData(consumer)
-					let data:{} = {}
+					this.setData(consumer);
 					consumer.bookings.forEach(booking => {
 						this.bookings.push(booking)
-
-					})
-					
-					this.dataSource.data.push(this.bookings)
+					})		
+					this.dataSource.data = this.data
+					console.log(this.data)
 					return consumer;
 				}));
 		})
@@ -51,11 +77,12 @@ export class VerReservasComponent implements OnInit {
 	show: boolean = false
 	showBookings: any = [];
 	calendar: boolean = true
+	data:any=[]
 	dataSource = new MatTableDataSource();
 	displayedColumns: string[] = ['bussiness', 'service', 'day', 'price'];
 
 	applyFilter(event: Event) {
-	  const filterValue = (event.target as HTMLInputElement).value;
+	  const filterValue = (event.target as HTMLInputElement).value || new String();
 	  this.dataSource.filter = filterValue.trim().toLowerCase();
 	}
 
@@ -64,7 +91,7 @@ export class VerReservasComponent implements OnInit {
 		private consumerService: ConsumerService,
 		private bookingService: ReservaService,
 		private negocioService: NegocioService
-	) { }
+	) { this.dataSource.data = this.data}
 
 	ngOnInit(): void {
 	}
@@ -144,6 +171,13 @@ export class VerReservasComponent implements OnInit {
 				window.location.reload();
 			});
 
+		}
+	}
+	changeView(){
+		if(this.calendar){
+			this.calendar = false;
+		}else{
+			this.calendar = true;
 		}
 	}
 }
