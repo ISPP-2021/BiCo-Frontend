f1f2752dccaf9607bb6e1606a445f73c06b4d390
Pepe <pepecesargarcia@gmail.com>
2021-05-14 23:23:52 +0200

    Tabla de reservas

--- BiCo/src/app/model/reserva.interface.ts
+++ BiCo/src/app/model/reserva.interface.ts
@@ -2,6 +2,7 @@ import { Negocio } from "./negocio.interface";
 import { Servicio } from "./service.interface";
 
 export interface Reserva {
+  id?: Number
 	emisionDate?: Date;
 	bookDate?: Date;
 	status?: String;
